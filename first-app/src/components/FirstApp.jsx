f28531a2a6dc498b0a3939ce7b8fd26e859ef7b6
lee <taeju95@gmail.com>
2026-02-13 10:12:20 +0900

    기초

---
+++ first-app/src/components/FirstApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './MyStyle.css';
+
+const FirstApp = () => {
+  return (
+    <div>
+        <h1 className='line'>FirstApp</h1>
+        <div style={{fontSize:'20px', color:'green', marginLeft:'300px'}}>Hello React</div>
+
+    </div>
+  )
+}
+
+export default FirstApp
---
+++ first-app/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import FirstApp from './components/FirstApp.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+    <FirstApp></FirstApp>
+  </StrictMode>,
+)
